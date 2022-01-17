bd6c6fdd6d9c52d8545334f9f2bacb141c954071
VishwaiOSDev <vishwa300499@icloud.com>
2022-01-17 15:29:49 +0530

    HotelList View Code Finished.

--- OYO/View/Hotels/HotelsListView.swift
+++ OYO/View/Hotels/HotelsListView.swift
@@ -2,71 +2,36 @@
 //  HotelsListView.swift
 //  OYO
 //
+
 //  Created by Vishwa  R on 20/12/21.
 //
-
 import SwiftUI
 
 struct HotelsListView: View {
     
-    @StateObject var viewModel : HotelsViewModel = HotelsViewModel()
-    @EnvironmentObject var authenticationViewModel : AuthenticationViewModel
+    @EnvironmentObject var viewModel : UserViewModel
     
     var body: some View {
         NavigationView {
-            if !viewModel.loading {
-                List(viewModel.hotels) { hotel in
-                    VStack {
-                        AsyncImage(url: URL(string: hotel.image)) { image in
-                            image.resizable()
-                        } placeholder: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(lineWidth: 2)
-                                Image(systemName: "photo.fill")
-                                    .font(.largeTitle)
-                            }
-                            .foregroundColor(.gray)
-                            .background(Color.white)
-                        }
-                        .frame(height: 200)
-                        .clipShape(RoundedRectangle(cornerRadius: 12))
-                        .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 4)
-                        HStack{
-                            VStack(alignment : .leading) {
-                                Text(hotel.hotel_name)
-                                    .font(.title3)
-                                    .fontWeight(.semibold)
-                                Text("₹\(hotel.cost_per_day)/ day")
-                                    .font(.callout)
-                            }
-                            Spacer()
-                            HStack(alignment : .center){
-                                Image(systemName: "star.fill")
-                                    .foregroundColor(.yellow)
-                                    .font(.system(size: 15))
-                                    .frame(width: 10)
-                                Text("\(hotel.rating)")
-                                    .font(.system(size: 15))
-                            }
-                        }
-                    }
-                    .padding(.vertical, 8)
-                    .background {
-                        NavigationLink(destination: HotelDetailsView(image: hotel.image, hotelName: hotel.hotel_name, description: hotel.description, price: hotel.cost_per_day, rating: hotel.rating, features: hotel.features)) {
-                            EmptyView()
-                        }
-                    }
+            List(viewModel.hotels) { hotel in
+                VStack(alignment : .leading) {
+                    Text(hotel.hotelName)
+                        .font(.title)
+                        .bold()
+                    Text("₹\(hotel.hotelPrice)/ day")
+                        .font(.title2)
+                    Text(hotel.hotelDescription)
+                        .font(.subheadline)
+                        .lineLimit(1)
                 }
-                .navigationBarTitle("Hotels")
-            } else {
-                ProgressView("Please wait...")
             }
+            .onAppear {
+                viewModel.getListOfHotels()
+            }.navigationTitle("Hotels")
         }
     }
+
 }
-
-
 
 struct HotelsListView_Previews: PreviewProvider {
     static var previews: some View {
@@ -74,3 +39,4 @@ struct HotelsListView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
     }
 }
+
